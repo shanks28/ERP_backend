509420b2a46cdce1d370f75adb223d6e27a3e88f
Saul Goodman <1rn21cs016.akash@gmail.com>
2025-05-05 23:44:41 +0530

    modified cors

--- src/main/java/com/example/ERP/Security/SecurityConfig.java
+++ src/main/java/com/example/ERP/Security/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(Arrays.asList("http://localhost:5174","https://erp-frontend-gafx.onrender.com/"));
+        configuration.setAllowedOriginPatterns(Arrays.asList("http://localhost:5174","https://erp-frontend-gafx.onrender.com"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(Collections.singletonList("*"));
         configuration.setExposedHeaders(Arrays.asList("Set-Cookie"));
