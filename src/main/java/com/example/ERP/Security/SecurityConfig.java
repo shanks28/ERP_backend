3aab9dd217131d4c14522050384d8ffcce7a1fbc
Saul Goodman <1rn21cs016.akash@gmail.com>
2025-05-14 18:49:05 +0530

    allowed precise origins over origin patters

--- src/main/java/com/example/ERP/Security/SecurityConfig.java
+++ src/main/java/com/example/ERP/Security/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(Arrays.asList(
+        configuration.setAllowedOrigins(Arrays.asList(
             "http://localhost:5174",
             "https://erp-frontend-gafx.onrender.com",
             "http://127.0.0.1:5500",
