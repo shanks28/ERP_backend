9a33523b2bc28fed8a858dcf863c9346d80d0782
Saul Goodman <1rn21cs016.akash@gmail.com>
2025-05-14 13:10:02 +0530

    fixed date issue

--- src/main/java/com/example/ERP/DTO/WithoutJobStatus.java
+++ src/main/java/com/example/ERP/DTO/WithoutJobStatus.java
@@ -5,6 +5,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import java.time.LocalDate;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -14,6 +16,8 @@ public class WithoutJobStatus {
     private Integer jobId;
     private String customerName;
     private String category;
+    
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
     private LocalDate jobDate;
     private Integer sellingPrice;
     private Integer costPrice;
@@ -21,21 +25,37 @@ public class WithoutJobStatus {
     private String jobParticulars;
     private String jobReference;
     private String boeSbNo;
+    
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
     private LocalDate boeSbDate;
+    
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
     private LocalDate arrivalDate;
+    
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
     private LocalDate tentativeClosureDate;
+    
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
     private LocalDate closedDate;
     private String billingStatus;
     private String apekshaInvoiceNo;
+    
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
     private LocalDate invoiceDate;
     private String courierTrackingNo;
     private String paymentStatus;
     private String action;
+    
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
     private LocalDate dateOfCourier;
+    
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
     private LocalDate dutyPaidDate;
+    
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
     private LocalDate clearanceDate;
+    
     @JsonProperty("isTemp")
     private boolean isTemp;
     private Integer tat;
-
 }
